5a9380d8106276865d6127134ffbd552037089d6
MrPink <will.pink@gmail.com>
2016-11-28 15:53:30 +0100

    not entirely happy with this, but its a start

--- api/db/setup.rb
+++ api/db/setup.rb
@@ -3,15 +3,13 @@ require "aws-sdk"
 require "redis"
 require "sidekiq"
 
-DB ||= if ENV["RACK_ENV"] == "test"
-         Sequel.connect(ENV["TEST_DATABASE_URL"])
-       elsif ENV.key?("RDS_HOSTNAME")
+DB ||= if ENV["RACK_ENV"] == "production"
          Sequel.connect(
            "postgres://#{ENV['RDS_USERNAME']}:#{ENV['RDS_PASSWORD']}@"\
            "#{ENV['RDS_HOSTNAME']}:#{ENV['RDS_PORT']}/#{ENV['RDS_DB_NAME']}"
          )
        else
-         Sequel.connect(ENV["DATABASE_URL"])
+         Sequel.connect("postgres://postgres@ticker-pg/my_api_#{ENV['RACK_ENV']}")
        end
 
 Sidekiq.configure_server do |config|
